43a42b7889ebbc9f0659e34fb587b16f470729fc
LabKitty <Shepard1>
2014-09-16 16:18:19 +0300

    Add Tingimuslaused

---
+++ index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+  <title>harjutus-4 - Andmetüübid (2)</title>
+</head>
+
+<body>
+	<h2>Tingimuslaused</h2>
+		
+		<?php
+			if (a = 4) {
+			echo "4 ja 4 on ühesugused numbrid";
+			}
+		?>
+		<?php
+			if (b = 5) {
+			echo "4 ja 4 on ühesugused numbrid";
+			}
+		?>
+		
+</body>
+</html>
